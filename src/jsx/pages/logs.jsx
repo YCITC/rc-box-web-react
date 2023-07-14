f2028e2a3ac409c63c119e12892369f4034f1302
Tid Huang <yesseecity@yahoo.com.tw>
2023-07-14 15:25:47 +0800

    Finish logs page

--- src/jsx/pages/logs.jsx
+++ src/jsx/pages/logs.jsx
@@ -1,24 +1,80 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import CssBaseline from '@mui/material/CssBaseline';
-import { Container, Typography, Box, Grid, Link, Checkbox, Avatar, Button} from '@mui/material';
+import axios from 'axios';
 
-
+import { Container } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useAuth } from '../hooks/use-auth.jsx';
 
 export default function DeliveryLogs() {
-  const navigate = useNavigate();
-  const [emailState, setEmailState] = useState('');
-  const [rememberState, setRememberState] = useState(false);
-  let email = null;
-  useEffect(()=>{
-    if (localStorage.getItem('userEmail') !== null) {
-      email = localStorage.getItem('userEmail');
-      setEmailState(localStorage.getItem('userEmail'))
-      setRememberState(true);
-    }
-  },[])
+  const auth = useAuth();
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'alias',
+      headerName: 'Alias',
+      width: 100,
+      editable: false,
+    },
+    {
+      field: 'deviceId',
+      headerName: 'Device ID',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'createdTime',
+      headerName: 'Time',
+      width: 400,
+      editable: true,
+    },
+  ];
 
-  const containerStyle =  {
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    if (auth.token === null) return; 
+    prepareLogs();
+  }, [auth.token])
+
+  const prepareLogs = async() => {
+    const devicesResponse = await axios.get('/api/devices/findAllByUser', {
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + auth.token
+      }
+    });
+    
+    const devices = devicesResponse.data;
+    // console.log('[DeliveryLogs] devices: ', devices);
+    let logsPromiseArray = [];
+    devices.forEach((device) => {
+      let logsPromise = axios.get('/api/log/get/'+device.deviceId, {
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + auth.token
+        }
+      });
+      logsPromiseArray.push(logsPromise);
+    });
+
+    let tempArray = [];
+    const responses = await Promise.all(logsPromiseArray);
+    responses.forEach((response) => {
+      response.data.map((log) => {
+        devices.forEach((device) => {
+          if (!device.alias || device.alias.length === 0) return;
+          log.alias = (device.deviceId === log.deviceId? device.alias: '');
+        });
+        log.createdTime = new Date(log.time).toLocaleString();
+      })
+      tempArray = tempArray.concat(response.data);
+    });
+    setLogs(tempArray);
+  }
+
+  const containerStyle = {
     // border: '1px dashed grey',
     width: '100%',
     position: 'absolute',
@@ -26,11 +82,11 @@ export default function DeliveryLogs() {
     // top: '50%',
     // translate: '-50% -50% 0',
   };
-  
+
   return (
-    <Container  component="div"  maxWidth="md" sx={containerStyle}>
-      <h1 style={{padding: '0 5px'}}>DeliveryLogs</h1>
-      <h2>filter</h2>
+    <Container component="div" maxWidth="md" sx={containerStyle}>
+      <h1 style={{ padding: '0 5px' }}>DeliveryLogs</h1>
+      <DataGrid rows={logs} columns={columns} getRowId={(row) => row.id}/>
     </Container>
   );
 }

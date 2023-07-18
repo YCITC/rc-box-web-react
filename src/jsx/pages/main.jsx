1a4366ffb8f694f6e8bb9a1a8688078d14d0a804
Tid Huang <yesseecity@yahoo.com.tw>
2023-07-18 17:00:46 +0800

    Add user guide in main page

--- src/jsx/pages/main.jsx
+++ src/jsx/pages/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import { styled } from '@mui/system';
 import { useTheme } from '@mui/material/styles';
-import { Container } from '@mui/material';
+import { useMediaQuery, Container, Box, Paper, Typography } from '@mui/material';
+
 
 export default function Main() {
   const theme = useTheme();
+  const mdUP = useMediaQuery((theme) => theme.breakpoints.up('md'));
 
+  const ImgComponent = ({imgSrc, sx}) => {
+    return <Paper elevation={3} sx={{maxWidth: "600px", width: "100%", marginBottom: '10px', ...sx}}>
+      <img src={imgSrc} style={{maxWidth: "inherit", width: "inherit"}} />
+    </Paper>
+  };
+  
   const containerStyle = {
     position: 'static',
     paddingTop: '10px',
@@ -23,12 +32,36 @@ export default function Main() {
     lineHeight: '1.5em',
   };
 
+  const renderUserGuide = () => {
+    if (mdUP === true) {
+      return <Box component="div">
+        <ImgComponent imgSrc="imgs/user-guide/account-popover.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-add.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-add-dialog.jpg" sx={{width: '400px'}} />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-edit.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-edit-dialog.jpg" sx={{width: '400px'}} />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-logs.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-logs-page.jpg" sx={{width: '400px'}} />
+      </Box>
+    } else {
+      return <Box component="div">
+        <ImgComponent imgSrc="imgs/user-guide/account-popover.mobile.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-add.mobile.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-add-dialog.jpg" sx={{width: '400px'}} />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-edit.mobile.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-box-edit-dialog.jpg" sx={{width: '400px'}} />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-logs.mobile.jpg" />
+        <ImgComponent imgSrc="imgs/user-guide/delivery-logs-page.jpg" sx={{width: '400px'}} />
+      </Box>
+    }
+  };
+
+
   return (
     <> 
       <Container component="div" maxWidth="md" sx={containerStyle}>
-        <h1 style={{ padding: '0 5px' }}>main</h1>
-        <p>Perhaps we could include a user guide here.</p>
-        <p>Maybe we can add a user guide to this page. </p>     
+        <h1 style={{ padding: '0 5px' }}>User Guide</h1>
+        {renderUserGuide()}
       </Container>
     </>
   );
